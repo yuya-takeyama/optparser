932a0d7f4204988f38fd6a36a5483f5b0713726a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-04-04 10:22:52 +0900

    Add some tests.

--- tests/OptParser/Tests/RuleTest.php
+++ tests/OptParser/Tests/RuleTest.php
@@ -27,4 +27,31 @@ class OptParser_Tests_RuleTest extends PHPUnit_Framework_TestCase
         $rule = new OptParser_Rule('abcde');
         $this->assertEquals(OptParser_Rule::VALUE_NONE, $rule->getMode());
     }
+
+    /**
+     * @test
+     */
+    public function getMode_should_be_the_value_set_on_constructor()
+    {
+        $rule = new OptParser_Rule('abcde', 'a', OptParser_Rule::VALUE_REQUIRED);
+        $this->assertEquals(OptParser_Rule::VALUE_REQUIRED, $rule->getMode());
+    }
+
+    /**
+     * @test
+     */
+    public function getDefault_should_true_by_default_if_VALUE_NONE_mode()
+    {
+        $rule = new OptParser_Rule('abcde', 'a', OptParser_Rule::VALUE_NONE);
+        $this->assertTrue($rule->getDefault());
+    }
+
+    /**
+     * @test
+     */
+    public function getDefault_should_be_the_value_set_on_constructor()
+    {
+        $rule = new OptParser_Rule('abcde', 'a', OptParser_Rule::VALUE_OPTIONAL, NULL, 'foo');
+        $this->assertEquals('foo', $rule->getDefault());
+    }
 }
