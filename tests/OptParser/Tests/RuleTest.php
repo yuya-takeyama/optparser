1cb322b172c30db12c9ce8c97d0574c41fe9862a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-04-04 09:31:14 +0900

    Add test.

---
+++ tests/OptParser/Tests/RuleTest.php
@@ -0,0 +1,21 @@
+<?php
+class OptParser_Tests_RuleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function getShortName_should_be_its_short_name()
+    {
+        $rule = new OptParser_Rule('a');
+        $this->assertEquals('a', $rule->getShortName());
+    }
+
+    /**
+     * @test
+     */
+    public function getLongName_should_be_its_short_name()
+    {
+        $rule = new OptParser_Rule('a', 'abcde');
+        $this->assertEquals('abcde', $rule->getLongName());
+    }
+}
